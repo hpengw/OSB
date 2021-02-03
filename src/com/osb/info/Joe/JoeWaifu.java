32c7fcfd274874abb98cabc8bf97879b71b96194
hpengw-MacBookPro <hanpeng2006@gmail.com>
2021-02-03 16:01:12 +1000

    Update 02/03

--- src/com/osb/info/Joe/JoeWaifu.java
+++ src/com/osb/info/Joe/JoeWaifu.java
@@ -1,15 +1,16 @@
 package com.osb.info.Joe;
 
-import com.osb.Main;
 import com.osb.Classes.Waifu;
+import com.osb.Main;
 
 public class JoeWaifu {
 
     public static void joeWF1() {
 
-        Waifu unknown = new Waifu();
+        Waifu Nancy = new Waifu();
 
         Main.println("# Information about Joe's 1st waifu: ");
+        Main.println("JJ Printers!!!");
 
     }
 
